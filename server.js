017d074e8bfdb212e005d351f5ae8c2c5c7a7bf0
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-11-07 10:25:04 -0300

    add endpoints

--- server.js
+++ server.js
@@ -1,5 +1,32 @@
 var express = require('express'),
-app = express(),
-port = process.env.PORT || 8080;
+  app = express(),
+  port = process.env.PORT || 8080;
 
-app.listen(port);
+app.get('/', function(req, res) {
+  console.log('GET /');
+  res.send('home');
+});
+
+app.get('/libertadores', function(req, res ){
+  console.log('GET /libertadores');
+  res.send('libertadores');
+});
+
+app.get('/sudamericana', function(req, res ){
+  console.log('GET /sudamericana');
+  res.send('sudamericana');
+});
+
+app.get('/superliga', function(req, res ){
+  console.log('GET /superliga');
+  res.send('superliga');
+});
+
+app.get('/argentina', function(req, res ){
+  console.log('GET /argentina');
+  res.send('argentina');
+});
+
+app.listen(port, function(){
+  console.log("listening on port 8080");
+});
