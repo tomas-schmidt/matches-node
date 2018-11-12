4e7a4a02f9964a7f1d8c95477edad0a69a72728a
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-11-12 11:24:15 -0300

    add mysql module and connection

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 8080;
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'pass',
+	database: 'db'
+});
+connection.connect();
+
+var baseQuery = "select h.team_name as 'home', v.team_name as 'visiting', match_date from matches m join teams h on (h.id_team = m.id_home_team) join teams v on (v.id_team = m.id_visiting_team) join competitions c on (m.id_competition = c.id_competition) where c.competition_name = "
+
 app.get('/', function(req, res) {
   console.log('GET /');
   res.send('home');
