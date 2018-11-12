1a70190831ae4f6b5114c7a0f8f667221cae42d4
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-11-12 13:40:30 -0300

    add getMatches

--- server.js
+++ server.js
@@ -7,12 +7,37 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: 'pass',
-	database: 'db'
+  password: 'pass',
+  database: 'db'
 });
 connection.connect();
 
-var baseQuery = "select h.team_name as 'home', v.team_name as 'visiting', match_date from matches m join teams h on (h.id_team = m.id_home_team) join teams v on (v.id_team = m.id_visiting_team) join competitions c on (m.id_competition = c.id_competition) where c.competition_name = "
+let baseQuery = "select h.team_name as 'home', v.team_name as 'visiting', match_date from matches m join teams h on (h.id_team = m.id_home_team) join teams v on (v.id_team = m.id_visiting_team) join competitions c on (m.id_competition = c.id_competition) where c.competition_name = "
+
+function getMatches(competition){
+	var deferred = $q.defer();
+	var matches = [];
+	var matchesQuery = queryBase + "'" + competencia + "'";
+	connection.query(matchesQuery, function(err, rows, fields) {
+		if(err) console.log(err);
+
+		if (rows) {
+			for (i=0; i < rows.length; i++) {
+				matches.push({
+					home: rows[i].home_team,
+					visiting: rows[i].visiting_team,
+					date: rows[i].match_date.getDate() + '/' + rows[i].match_date.getMonth(),
+					time: rows[i].match_date.getHours() + ':' + rows[i].match_date.getMinutes()
+				});
+			}
+      console.log('Matches found:')
+      console.log(matches);
+    } else console.log("No matches found")
+	});
+	deferred.resolve(matches);
+	return deferred.promise;
+}
+
 
 app.get('/', function(req, res) {
   console.log('GET /');
@@ -21,7 +46,10 @@ app.get('/', function(req, res) {
 
 app.get('/libertadores', function(req, res ){
   console.log('GET /libertadores');
-  res.send('libertadores');
+  var promise = obtenerPartidos('Copa Libertadores');
+	promise.then(function(data){
+		res.send(data);
+	});
 });
 
 app.get('/sudamericana', function(req, res ){
