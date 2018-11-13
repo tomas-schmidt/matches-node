ea0619897b014c45fc34ab403f314163f8b6cdf2
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-11-13 13:57:36 -0300

    add interpolation, arrow functions and groom code

--- server.js
+++ server.js
@@ -12,24 +12,24 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-let baseQuery = "select h.team_name as 'home', v.team_name as 'visiting', match_date from matches m join teams h on (h.id_team = m.id_home_team) join teams v on (v.id_team = m.id_visiting_team) join competitions c on (m.id_competition = c.id_competition) where c.competition_name = "
+let baseQuery = "select h.team_name as 'home', v.team_name as 'visiting', match_date from matches m join teams h on (h.id_team = m.id_home_team) join teams v on (v.id_team = m.id_visiting_team) join competitions c on (m.id_competition = c.id_competition) where c.competition_name = ";
 
-function getMatches(competition){
+function getMatches(competition) {
 	let deferred = $q.defer();
 	let matches = [];
-	let matchesQuery = baseQuery + "'" + competition + "'";
-	connection.query(matchesQuery, function(err, rows, fields) {
+	let matchesQuery = `${baseQuery}'${competition}'`;
+	connection.query(matchesQuery, (err, rows, fields) => {
 		if(err) console.log(err);
 		if (rows) {
-			for (i=0; i < rows.length; i++) {
+			for (let i = 0; i < rows.length; i ++) {
 				matches.push({
 					home: rows[i].home_team,
-					visiting: rows[i].visiting_team,
-					date: rows[i].match_date.getDate() + '/' + rows[i].match_date.getMonth(),
-					time: rows[i].match_date.getHours() + ':' + rows[i].match_date.getMinutes()
+          visiting: rows[i].visiting_team,
+          date: `${rows[i].match_date.getDate()}/${rows[i].match_date.getMonth()}`,
+					time: `${rows[i].match_date.getHours()}/${rows[i].match_date.getMinutes()}`
 				});
 			}
-      console.log('Matches found:')
+      console.log('Matches found:');
       console.log(matches);
     } else console.log("No matches found")
 	});
@@ -38,46 +38,36 @@ function getMatches(competition){
 }
 
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   console.log('GET /');
-  var promise = getMatches('*');
-	promise.then(function(data){
-		res.send(data);
-	});
+  let promise = getMatches('*');
+	promise.then(data => { res.send(data); });
 });
 
-app.get('/libertadores', function(req, res ){
+app.get('/libertadores', (req, res) => {
   console.log('GET /libertadores');
-  var promise = getMatches('Copa Libertadores');
-	promise.then(function(data){
-		res.send(data);
-	});
+  let promise = getMatches('Copa Libertadores');
+	promise.then(data => { res.send(data); });
 });
 
-app.get('/sudamericana', function(req, res ){
+app.get('/sudamericana', (req, res) => {
   console.log('GET /sudamericana');
-  var promise = getMatches('Sudamericana');
-	promise.then(function(data){
-		res.send(data);
-	});
+  let promise = getMatches('Sudamericana');
+	promise.then(data => { res.send(data); });
 });
 
-app.get('/superliga', function(req, res ){
+app.get('/superliga', (req, res) => {
   console.log('GET /superliga');
-  var promise = getMatches('Superliga');
-	promise.then(function(data){
-		res.send(data);
-	});
+  let promise = getMatches('Superliga');
+	promise.then(data => { res.send(data); });
 });
 
-app.get('/argentina', function(req, res ){
+app.get('/argentina', (req, res) => {
   console.log('GET /argentina');
-  var promise = getMatches('Argentina');
-	promise.then(function(data){
-		res.send(data);
-	});
+  let promise = getMatches('Argentina');
+	promise.then(data => { res.send(data); });
 });
 
-app.listen(port, function(){
+app.listen(port, () => {
   console.log("listening on port 8080");
 });
