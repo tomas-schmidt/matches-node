212c7e429b147891388f28ea368461ce023b60cf
tomas-schmidt <tomas.schmidt@wolox.com.ar>
2018-11-14 11:59:15 -0300

    fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Database = require('./database/database.js');
 let express = require('express'),
   app = express(),
   port = process.env.PORT || 8080;
-  
+
 let database = new Database({ host: '127.0.0.1', port: 3306, user: 'root', password: 'pass', database: 'db' });
 let baseQuery = "select h.team_name as 'home', v.team_name as 'visiting', match_date from matches m join teams h on (h.id_team = m.id_home_team) join teams v on (v.id_team = m.id_visiting_team) join competitions c on (m.id_competition = c.id_competition) where c.competition_name = ";
 let todayQuery = "select h.team_name as 'home', v.team_name as 'visiting', match_date, competition_name from matches m join teams h on (h.id_team = m.id_home_team) join teams v on (v.id_team = m.id_visiting_team) join competitions c on (m.id_competition = c.id_competition) where match_date = (select CURDATE())"
@@ -12,23 +12,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/libertadores', (req, res) => {
-  database.query(`${baseQuery}'Copa Libertadores'`).then( rows => { res.send(rows) });
+  database.query(`${baseQuery}'Copa Libertadores'`).then(rows => { res.send(rows) });
 });
 
 app.get('/sudamericana', (req, res) => {
-  database.query(`${baseQuery}'Copa Sudamericana'`).then( rows => { res.send(rows) });
+  database.query(`${baseQuery}'Copa Sudamericana'`).then(rows => { res.send(rows) });
 });
 
 app.get('/superliga', (req, res) => {
-  database.query(`${baseQuery}'Superliga'`).then( rows => { res.send(rows) });
+  database.query(`${baseQuery}'Superliga'`).then(rows => { res.send(rows) });
 });
 
 app.get('/argentina', (req, res) => {
-  database.query(`${baseQuery}'Copa Argentina'`).then( rows => { res.send(rows) });
+  database.query(`${baseQuery}'Copa Argentina'`).then(rows => { res.send(rows) });
 });
 
 app.get('/competitions', (req, res) => {
-  database.query('SELECT * FROM competitions').then( rows => { res.send(rows) });
+  database.query('SELECT * FROM competitions').then(rows => { res.send(rows) });
 });
 
 app.listen(port, () => {
