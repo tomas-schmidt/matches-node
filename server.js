460252f095dcd1c2edb0072a1da9c0c57ad52b9b
Tomas <tomas.schmidt@wolox.com.ar>
2018-11-18 17:19:45 -0300

    add queries module

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const Database = require('./database/database.js');
 const Environment = require('./environments/environment.js');
-let express = require('express'), app = express(), port = Environment.port;
-
+let queries = require('./database/queries.js');
 let database = new Database(Environment.databaseConfig);
-let baseQuery = "select id_match as 'id', h.team_name as 'homeTeam', h.team_logo as 'homeTeamLogo', v.team_name as 'visitingTeam', v.team_logo as 'visitingTeamLogo',  TIME_FORMAT(match_date," + '"%H:%i"' + ") as 'time', DATE_FORMAT(match_date," + '"%d/%m"' + ") as 'day' from matches m join teams h on (h.id_team = m.id_home_team) join teams v on (v.id_team = m.id_visiting_team) join competitions c on (m.id_competition = c.id_competition) where c.competition_name = ";
-let todayQuery = "select id_match as 'id', h.team_name as 'homeTeam', h.team_logo as 'homeTeamLogo', v.team_name as 'visitingTeam', v.team_logo as 'visitingTeamLogo', TIME_FORMAT(match_date," + '"%H:%i"' + ") as 'time', DATE_FORMAT(match_date," + '"%d/%m"' + ") as 'day', competition_name from matches m join teams h on (h.id_team = m.id_home_team) join teams v on (v.id_team = m.id_visiting_team) join competitions c on (m.id_competition = c.id_competition) where match_date = (select CURDATE())"
+let express = require('express'), app = express(), port = Environment.port;
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', Environment.frontEndUrl);
@@ -12,27 +10,27 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  database.query(todayQuery).then( rows => { res.send(rows) });
+  database.query(queries.today).then( rows => { res.send(rows) });
 });
 
 app.get('/libertadores', (req, res) => {
-  database.query(`${baseQuery}'Copa Libertadores'`).then(rows => { res.send(rows) });
+  database.query(`${queries.base}'Copa Libertadores'`).then(rows => { res.send(rows) });
 });
 
 app.get('/sudamericana', (req, res) => {
-  database.query(`${baseQuery}'Copa Sudamericana'`).then(rows => { res.send(rows) });
+  database.query(`${queries.base}'Copa Sudamericana'`).then(rows => { res.send(rows) });
 });
 
 app.get('/superliga', (req, res) => {
-  database.query(`${baseQuery}'Superliga'`).then(rows => { res.send(rows) });
+  database.query(`${queries.base}'Superliga'`).then(rows => { res.send(rows) });
 });
 
 app.get('/argentina', (req, res) => {
-  database.query(`${baseQuery}'Copa Argentina'`).then(rows => { res.send(rows) });
+  database.query(`${queries.base}'Copa Argentina'`).then(rows => { res.send(rows) });
 });
 
 app.get('/competitions', (req, res) => {
-  database.query('SELECT * FROM competitions').then(rows => { res.send(rows) });
+  database.query(queries.competitions).then(rows => { res.send(rows) });
 });
 
 app.listen(port, () => {
